b41995b61c979b06141cfca5467b41ecfaef06ac
Abhi Pandey <pandeyabhi142002@gmail.com>
2023-06-30 12:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+function updateTimer() {
+        var now = new Date();
+        var date = now.toLocaleDateString();
+        var time = now.toLocaleTimeString([], {
+          hour: 'numeric',
+          minute: '2-digit',
+          second: '2-digit',
+          hour12: true
+        });
+
+        var timerElement = document.getElementById('timer');
+        timerElement.textContent = date + ', ' + time;
+
+        setTimeout(updateTimer, 1000);
+      }
+
+      updateTimer();
